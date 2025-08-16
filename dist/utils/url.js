e9acc982ecc67e456df914ee5281de6dbbd92561
vasu kochhar <vasu.kochhar@example.com>
2025-08-16 15:53:49 +0530

    chore: ignore env, node_modules, dist, uploads; keep uploads/.gitkeep

--- dist/utils/url.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.absoluteUrl = absoluteUrl;
-function absoluteUrl(relativePath) {
-    const base = (process.env.PUBLIC_BASE_URL || '').replace(/\/$/, '');
-    const rel = relativePath.startsWith('/') ? relativePath : `/${relativePath}`;
-    return `${base}${rel}`;
-}
